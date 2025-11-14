53cf45613340cff02741c4e6e0a1916db287f630
siddhant <amlucifer7@gmail.com>
2025-11-14 15:01:08 +0530

    requiremets added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="med-chat",
+    version="0.1.0",
+    author="siddhant",
+    author_email="amlucifer7@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
