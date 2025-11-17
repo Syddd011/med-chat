85b2a3f82971f691ecda1a593958d4b43292aae9
siddhant <amlucifer7@gmail.com>
2025-11-17 17:18:25 +0530

    Initial commit

--- template.sh
+++ template.sh
@@ -1,12 +1,16 @@
+# Creating directory 
 mkdir -p src
-mkdir -p research
+mkdir -p research 
+
+# Creating files
 touch src/__init__.py
-touch src/helper.py
+touch src/helper.py 
 touch src/prompt.py
-touch .env
+touch .env 
 touch setup.py
 touch app.py
-touch research/trials.ipynb
+touch research/trials.ipynb 
 touch requirements.txt
 
-echo "directory created"
+
+echo "Directory and files created successfully!."
